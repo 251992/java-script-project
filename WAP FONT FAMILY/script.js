77d6a0f50ec3a88132f382bd892350fe948a73c2
unknown <admin@chetuiwk0699.chetu.com>
2021-08-24 07:43:55 -0700

    initial commit

---
+++ WAP FONT FAMILY/script.js
@@ -0,0 +1,23 @@
+function js_style() 
+{
+
+ text.style.fontSize = "20px";
+ text.style.fontFamily = "Comic Sans MS";
+ text.style.color = "red";
+}
+function js_style1() 
+{
+
+ text.style.fontSize = "30px";
+ text.style.fontFamily = "Comic Sans MS";
+ text.style.color = "green";
+}
+function js_style2() 
+{
+
+ text.style.fontSize = "40px";
+ text.style.fontFamily = "Comic Sans MS";
+ text.style.color = "blue";
+
+
+}
